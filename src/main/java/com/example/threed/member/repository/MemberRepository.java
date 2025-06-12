ca1d1237dab87bc027e612f61c140a06d51261a3
hyun95 <gustlr8399@naver.com>
2025-06-12 15:57:23 +0900

    feat: 구글 로그아웃 구현 및 테스트

--- src/main/java/com/example/threed/member/repository/MemberRepository.java
+++ src/main/java/com/example/threed/member/repository/MemberRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 	Optional<Member> findByAuthProviderProviderTypeAndAuthProviderProviderId(ProviderType providerType, String providerId);
+	Optional<Member> findByRefreshToken_Value(String refreshTokenValue);
 }
